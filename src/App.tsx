e99897df71e6a75d299cdae40e4357f8ece964f2
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-10 18:04:44 +0530

    tailwindcss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes} from 'react-router-dom'
+import { Home } from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Home 
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
   )
 }
 
